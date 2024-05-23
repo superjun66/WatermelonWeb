cb3bd9aef3b9cb467899cda87246b1c455f0e4aa
wujun <jun.wu@jiamiantech.com>
2024-05-23 16:10:54 +0800

    1

---
+++ src/settings.dc073.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","wall","fruit"],collisionMatrix:[[true],[false,false,true],[false,true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/WatermelonScene.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"a4789",main:"a5dc1"}};
